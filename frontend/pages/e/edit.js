8bbf0de059f36f5002c8cfc9c2054a644027dc65
nayan <nayanansh@gmail.com>
2023-08-18 11:22:30 +0530

    Update edit.js

--- frontend/pages/e/edit.js
+++ frontend/pages/e/edit.js
@@ -14,6 +14,7 @@ import { addPost } from '../../server/blogs';
 import { useRouter } from 'next/navigation';
 import Root from "../../components/Root";
 import Head from "next/head";
+import Script from "next/script";
 
 
 /*
